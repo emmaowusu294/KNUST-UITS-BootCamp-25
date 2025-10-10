a2f403fc51111087cf8631ff5ac1f4b37ea7663a
4Ts-code <eo8407272@gmail.com>
2025-10-10 12:20:22 +0000

    SCORES

---
+++ MiniProject/WebApplication1/WebApplication1/Controllers/ScoresController.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ScoresController : Controller
+    {
+
+        private DatabaseService _dbService;
+
+        public ScoresController(DatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MiniProject/WebApplication1/WebApplication1/Controllers/StudentController.cs
+++ MiniProject/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -1,34 +1,99 @@
 ﻿using ConsoleApp2;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebApplication1.Controllers
 {
     public class StudentController : Controller
     {
-        private DatabaseService _dbService;
+        private readonly DatabaseService _dbService;
 
         public StudentController()
         {
             _dbService = new DatabaseService();
         }
+
+        // 🔹 LIST ALL STUDENTS
         public IActionResult Index()
         {
             List<Student> students = _dbService.GetStudents();
             return View(students);
         }
 
+        // 🔹 ADD (GET)
         public IActionResult Add()
         {
             return View();
         }
 
-        [HttpPost] 
+        // 🔹 ADD (POST)
+        [HttpPost]
         public IActionResult Add([FromForm] Student request)
         {
-            _dbService.AddStudent(request.FirstName, request.LastName, 
+            _dbService.AddStudent(request.FirstName, request.LastName,
                 request.Gender, request.BirthDate);
 
             return RedirectToAction(nameof(Index));
         }
+
+        // 🔹 EDIT (GET)
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _dbService.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // 🔹 EDIT (POST)
+        [HttpPost]
+        public IActionResult Edit([FromForm] Student request)
+        {
+            if (request.StudentId == null)
+                return BadRequest("Student ID is missing");
+
+            _dbService.UpdateStudent(request.StudentId.Value,
+                request.FirstName, request.LastName,
+                request.Gender, request.BirthDate);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // 🔹 Delete (GET)
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var student = _dbService.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // 🔹 Delete (POST)
+        [HttpPost]
+        public IActionResult Delete([FromForm] Student request)
+        {
+            if (request.StudentId == null)
+                return BadRequest("Student ID is missing");
+
+            _dbService.DeleteStudent(request.StudentId.Value);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //FILTER (GET)
+        public IActionResult Filter()
+        {
+            return View();
+        }
+
+       
+
     }
 }
