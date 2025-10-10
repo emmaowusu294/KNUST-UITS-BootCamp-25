f9162715761caf3a12934fc0bd319c2a881c81f9
4Ts-code <eo8407272@gmail.com>
2025-10-10 10:13:31 +0000

    Changes

---
+++ MiniProject/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private DatabaseService _dbService;
+
+        public StudentController()
+        {
+            _dbService = new DatabaseService();
+        }
+        public IActionResult Index()
+        {
+            List<Student> students = _dbService.GetStudents();
+            return View(students);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Add([FromForm] Student request)
+        {
+            _dbService.AddStudent(request.FirstName, request.LastName, 
+                request.Gender, request.BirthDate);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MiniProject/WebApplication1/WebApplication1/Services/DatabaseService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApp2
+{
+    public class DatabaseService
+    {
+        string _connStr;
+
+        public DatabaseService()
+        {
+            _connStr = @"Server=4TS\SQLEXPRESS;Database=School;Trusted_Connection=True;TrustServerCertificate=True";
+        }
+
+        public void AddStudent(string fname, string lname,
+            char gender, DateOnly birthDate)
+        {
+            SqlConnection conn = new SqlConnection(_connStr);
+            string sql = @"INSERT INTO Student (FirstName, LastName, Gender, BirthDate) 
+                            VALUES (@fname, @lname, @gender, @bdate)";
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@fname", fname);
+            cmd.Parameters.AddWithValue("@lname", lname);
+            cmd.Parameters.AddWithValue("@gender", gender);
+            cmd.Parameters.AddWithValue("@bdate", birthDate);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine("Student added successfully");
+        }
+
+        public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            SqlConnection conn = new SqlConnection(_connStr);
+            string sql = "SELECT * FROM Student";
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while(reader.Read())
+            {
+                Student temp = new Student();
+                temp.StudentId = reader.GetInt32(0);
+                temp.FirstName = reader.GetString(1);
+                temp.LastName = reader.GetString(2);
+                temp.Gender = reader.GetString(3)[0];
+                temp.BirthDate = DateOnly.FromDateTime(reader.GetDateTime(4));
+
+                students.Add(temp);
+            }
+
+            conn.Close();
+
+            return students;
+
+
+        }
+    }
+
+    public class Student
+    {
+        public int? StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName{ get; set; }
+        public Char Gender{ get; set; }
+        public DateOnly BirthDate{ get; set; }
+    }
+}
